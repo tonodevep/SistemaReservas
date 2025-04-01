4166e7dcb6538cc4c84e007c3a590631f4345729
Toño Tejera <tonodevep@gmail.com>
2025-04-01 16:25:31 +0100

    Create ClienteNoEncontradoException.java
    Creación de una nueva excepción personalizada

---
+++ Habitación/Excepciones/ClienteNoEncontradoException.java
@@ -0,0 +1,8 @@
+package Excepciones;
+
+public class ClienteNoEncontradoException extends Exception{
+    
+    public ClienteNoEncontradoException(String message) {
+        super(message);
+    }
+}
