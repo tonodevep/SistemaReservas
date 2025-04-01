8a7c4ab946f09fd65183ed459d948f37c09fe859
Juniuun <sarafox_2006@hotmail.com>
2025-04-01 12:26:54 +0100

    Create ReservaNoDisponibleException.java
    Creación de la carpeta excepciones más la implementación de una excepción personalizada

---
+++ Habitación/Excepciones/ReservaNoDisponibleException.java
@@ -0,0 +1,8 @@
+package Excepciones;
+
+public class ReservaNoDisponibleException extends Exception{
+    
+    public ReservaNoDisponibleException(String message) {
+        super(message);
+    }
+}
