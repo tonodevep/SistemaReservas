e048c380013c9aacef8ae760df8dda60ae6fbc90
Juniuun <sarafox_2006@hotmail.com>
2025-04-02 22:10:50 +0100

    Update ControladorCliente.java
    update definitivo de ControladorCliente... ahora es personal (se me olvidaron añadirle notas)

--- Habitación/Controlador/ControladorCliente.java
+++ Habitación/Controlador/ControladorCliente.java
@@ -8,17 +8,19 @@ import Excepciones.ClienteNoEncontradoException;
 
 public class ControladorCliente {
     
-    private List<Cliente> clientes = new ArrayList<>(); // Añade esto al inicio de la clase
+    private List<Cliente> clientes = new ArrayList<>();
 
-    // Constructor para inicializarlo:
+    // Constructor para iniciar clientes:
     public ControladorCliente() {
         this.clientes = new ArrayList<>();
     }
-    
+
+    //Método para agregar a los clientes al array
     public void agregarCliente(Cliente cliente) {
         this.clientes.add(cliente);
     }
-    
+
+    //Método para buscar clientes
     public Cliente buscarCliente(int IDCliente) throws ClienteNoEncontradoException {
     for (Cliente c : clientes) {
         if (c.getIDCliente() == IDCliente) {
@@ -27,26 +29,29 @@ public class ControladorCliente {
     }
     throw new ClienteNoEncontradoException("Cliente con ID " + IDCliente + " no encontrado.");
 }
-
+    //Getter de reservas activas
     public List<Reserva> getReservasActivas(int IDCliente) throws ClienteNoEncontradoException {
         Cliente cliente = buscarCliente(IDCliente);
         return cliente != null ? cliente.getReservasActivas() : new ArrayList<>();
     }
 
+    //Método que verifica si es posible hacer una reserva
     public boolean puedeHacerReserva(int IDCliente) throws ClienteNoEncontradoException {
         Cliente cliente = buscarCliente(IDCliente);
         return cliente != null && cliente.puedeReservar();
     }
 
+    //Método que agrega reservas
     public void agregarReserva(int IDCliente, Reserva reserva) throws ClienteNoEncontradoException {
         Cliente cliente = buscarCliente(IDCliente);
         if (cliente == null) {
             throw new ClienteNoEncontradoException("No se ha podido encontrar al cliente.");
         }
         cliente.agregarReserva(reserva);
-        System.out.println("\n Reserva con éxito");
+        System.out.println("\n Reserva hecha con éxito");
     }
 
+    //Método para finalizar la reserva
     public void finalizarReserva(int IDCliente, Reserva reserva) throws ClienteNoEncontradoException {
         Cliente cliente = buscarCliente(IDCliente);
         if (cliente != null) {
@@ -55,7 +60,7 @@ public class ControladorCliente {
             throw new ClienteNoEncontradoException("No se ha podido encontrar al cliente, por lo que no se ha podido finalizar ninguna reserva");
         }
     }
-
+    //Getter
     public List<Cliente> getTodosClientes() {
         return new ArrayList<>(clientes);
     }
