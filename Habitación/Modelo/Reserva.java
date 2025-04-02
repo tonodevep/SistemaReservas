f6cdb5333884dede98f07c619752c177e8a24ffa
Juniuun <sarafox_2006@hotmail.com>
2025-04-02 22:19:39 +0100

    Update Reserva.java
    Update final de Reserva

--- Habitación/Modelo/Reserva.java
+++ Habitación/Modelo/Reserva.java
@@ -13,25 +13,31 @@ public class Reserva {
     private static int IDReservaCounter = 0;
     private int IDReserva;
     private List<Habitacion> habitacionReservada;
+    private Cliente cliente;
     private LocalDate checkIn;
     private LocalDate checkOut;
     private double precioTotal;
 
     //Constructor
-    public Reserva(LocalDate checkIn, LocalDate checkOut, double precioTotal){
+    public Reserva(int IDReserva, Habitacion habitacion, Cliente cliente, LocalDate checkIn, LocalDate checkOut){
         this.IDReserva = ++IDReservaCounter;
         this.habitacionReservada = new ArrayList<>();
+        this.habitacionReservada.add(habitacion);
+        this.cliente = cliente;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
         this.precioTotal = calcularPrecioTotal();
     }
 
     //Método que calcula el precio total dependiendo el tipo de habitación y la estancia
-    private double calcularPrecioTotal(){
-
-        long noches = ChronoUnit.DAYS.between(checkIn, checkOut);
-        return noches * Habitacion.getTipo().getPrecioPorNoche();
+    private double calcularPrecioTotal() {
+    long noches = ChronoUnit.DAYS.between(checkIn, checkOut);
+    double precio = 0.0;
+    for (Habitacion hab : habitacionReservada) {
+        precio += noches * hab.getTipo().getPrecioPorNoche(); // Usar la instancia 'hab'
     }
+    return precio;
+}
 
     //Getters
     public int getIDReserva() {
@@ -40,6 +46,10 @@ public class Reserva {
 
     public List<Habitacion> getHabitacionReservada() {
         return habitacionReservada;
+    }
+    
+    public Cliente getCliente() {
+        return cliente;
     }
 
     public LocalDate getCheckIn() {
@@ -63,9 +73,8 @@ public class Reserva {
         this.checkOut = checkOut;
     }
 
-    public void setPrecioTotal(int precioTotal) {
+    public void setPrecioTotal(double precioTotal) {
         this.precioTotal = precioTotal;
     }
         
 }
-
